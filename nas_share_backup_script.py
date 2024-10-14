e421d0660e395ef3b5827c1258cacccc92217302
Michel <michelholmgren@gmail.com>
2024-10-14 05:31:34 +0200

    correction return value of telegram_bot script call + all logging together at the end

--- nas_share_backup_script.py
+++ nas_share_backup_script.py
@@ -17,18 +17,15 @@ process = subprocess.run(rsync_command, capture_output=True, text=True)
 #summary_content preparation
 summary_content = process.stdout + process.stderr
 
-#log summary
-with open(log_file, 'a') as log:
-  log.write(f"Backup on: \{datetime.now()\}\\n")
-  log.write(summary_content)
-
 #Error check and send message with telegram bot
 if process.returncode is not 0:
-  send_message(f"FAILED auto-backup NAS '/share/'-->'/share_backup/': \n errorcode: {process.returncode} \n{summary_content}")
-else
-  send_message(f"SUCCESFULL auto-backup  '/share/'-->'/share_backup/' : \n {summary_content}")
+  telegram_response = send_message(f"FAILED auto-backup NAS '/share/'-->'/share_backup/': \n errorcode: {process.returncode} \n{summary_content}")
+else:
+  telegram_response = send_message(f"SUCCESFULL auto-backup  '/share/'-->'/share_backup/' : \n {summary_content}")
 
+#log summary
 with open(log_file, 'a') as log:
-    log.write(f"telegram_bot.py return info: {send_message}")
-#logging telegram_bot.py return for easy follow-up when having malfunctions
+  log.write(f"Backup on: {datetime.now()}\n")
+  log.write(f"telegram_bot.py api response: {telegram_response}")
+  log.write(summary_content)
 
