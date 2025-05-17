b028f73b638f8e4201476fc217fca2796cc0b32a
Michel <michelholmgren@gmail.com>
2025-05-17 14:26:36 +0200

    Update telegram_bot.py

--- telegram_bot.py
+++ telegram_bot.py
@@ -7,14 +7,19 @@ import requests
 bot_token = os.environ.get("BOT_TOKEN")
 chat_id = os.environ.get("CHAT_ID")
 # bot_token&chat_id has been stored on linux mint as sensitive data by using the environment variables
+# If auto setup for configuration of variables wasn't used. Manually is also possible:
 # use the following to store you sensitive date securely onto the OS. 
-  # run in terminal: "nano ~/.env_telegram"
+  # run in terminal: "nano ~/.env_backup_automated"
   # place:
     # export BOT_TOKEN='your_bot_token_here'
     # export CHAT_ID='your_chat_id_here'
+    # export BACKUP_SOURCE='$BACKUP_SOURCE'
+    # export BACKUP_DESTINATION='$BACKUP_DESTINATION'
+    # export BACKUP_UUID='$BACKUP_UUID'
+    # export BACKUP_LOG_FILE='$BACKUP_LOG_FILE'
   # ctrl+o (for save) & ctrl-x (for exit)
-  # run (in terminal): "source ~/.env_telegram"
-  # run (in terminal): "chmod 600 ~/.env_telegram"
+  # run (in terminal): "source ~/.env_backup_automated"
+  # run (in terminal): "chmod 600 ~/.env_backup_automated
 
 
 def send_message(msg):
