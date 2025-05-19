bd88182f6f24fed2a4d3a0b5df0b75ac82583073
Michel <michelholmgren@gmail.com>
2025-05-19 10:45:31 +0200

    Update telegram_bot.py
    changed requests.get into requests.post

--- telegram_bot.py
+++ telegram_bot.py
@@ -12,7 +12,7 @@ def send_message(msg):
     params = {'chat_id': chat_id, 'text': msg}
 
     # Make the GET request and capture the response
-    response = requests.get(url, params=params)
+    response = requests.post(url, params=params)
     
     # Check if the request was successful
     if response.status_code != 200:
